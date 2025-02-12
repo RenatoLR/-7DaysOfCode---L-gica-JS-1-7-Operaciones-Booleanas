43361bc26d9b7557ea33b8dcdc7ae223507a8639
Renato Paolo Cesar La Rosa Diaz <ing.rlarosa97@hotmail.com>
2025-02-12 15:38:15 -0500

    Add files via upload

---
+++ Operaciones_booleanas.js
@@ -0,0 +1,16 @@
+let numeroUn = 1;
+let stringUn = "1";
+let numeroTreinta = 30;
+let stringTreinta = "30";
+let numeroDiez = 10;
+let stringDiez = "10";
+
+console.log ((numeroUn === stringUn)
+? `Las variables numeroUn y stringUn tienen el mismo valor y son del mismo tipo`:
+ `Las variables numeroUn y stringUn tienen el mismo valor, pero pertenecen a tipos distintos.`);
+console.log ((numeroTreinta === stringTreinta)
+?`Las variables numeroTreinta y stringTreinta tienen el mismo valor y son del mismo tipo`
+: `Las variables numeroTreinta y stringTreinta tienen el mismo valor, pero pertenecen a tipos distintos.`);
+console.log ((numeroDiez  === stringDiez)
+? `Las variables numeroDiez y stringDiez tienen el mismo valor y son del mismo tipo`
+: `Las variables numeroDiez y stringDiez tienen el mismo valor, pero pertenecen a tipos distintos.`);
